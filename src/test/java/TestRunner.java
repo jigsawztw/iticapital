86bb4f3539bcf8a81d27bdce7438afe76dcadd06
jigsawztw <bamby4a364@gmail.com>
2019-06-17 19:45:22 +0300

    initial project version

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,16 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Nikolay Streltsov on 02.09.2018
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features/",
+        glue = {"com.aplana.chameleon"},
+        plugin = {"pretty"}
+)
+public class TestRunner {
+
+}
